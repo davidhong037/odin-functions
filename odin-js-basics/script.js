43b46813649b41406274ac49845b60436fbdc026
davidhong037 <phazex1@gmail.com>
2024-03-12 14:21:32 -0400

    Adding function assignments

---
+++ odin-js-basics/script.js
@@ -0,0 +1,16 @@
+function add7(num) {
+    let result = num + 7;
+    return result;
+}
+console.log(2)
+
+function multiply(a, b) {
+    let result = a * b;
+    return result;
+}
+
+function capitalize(string) {
+    let string = "";
+    let result = string.charAt(0).toUppercase()
+    return result;
+}
